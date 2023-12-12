c629c2046aa6ddc07882dcb9bab2ff257a5d5f07
Sudiddo <su.diddo@gmail.com>
2023-12-12 15:54:04 +0700

    add github repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,19 @@ export default function Home() {
     <main className="flex justify-center p-5">
       <div className="md:max-w-7xl w-full">
         {/* Header */}
-        <div className="bg-black rounded-lg p-3 w-fit mb-5">
-          <h1 className="font-bold text-2xl text-white">
-            360 Tour Available Plugins
-          </h1>
+        <div className="flex flex-row w-full justify-between items-center">
+          <div className="bg-black rounded-lg p-3 w-fit mb-5">
+            <h1 className="font-bold text-2xl text-white">
+              360 Tour Available Plugins
+            </h1>
+          </div>
+          <a
+            href="https://github.com/sudiddo/react-360-plugins"
+            target="_blank"
+            className="underline font-medium text-lg"
+          >
+            Github Repo
+          </a>
         </div>
         {/* Content */}
         <div className="mt-10 flex flex-row ">
