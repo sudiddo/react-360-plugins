9d3f449afcc84c59f75511cc0306db44d9a8434d
Sudiddo <su.diddo@gmail.com>
2023-12-15 15:54:48 +0700

    update photo and link

--- src/modules/components/ComparisonTable.tsx
+++ src/modules/components/ComparisonTable.tsx
@@ -6,57 +6,67 @@ function ComparisonTable() {
       <table className="min-w-full w-full table-auto border-collapse border border-gray-200 text-base">
         <thead>
           <tr className="bg-gray-100 rounded">
-            <th className="border border-gray-300 px-4 py-2 text-white bg-black font-bold">
+            <th className="border border-gray-300 md:px-4 px-2 py-2 text-white bg-black font-bold">
               Feature
             </th>
-            <th className="border border-gray-300 px-4 py-2 text-white bg-black font-bold">
+            <th className="border border-gray-300 md:px-4 px-2 py-2 text-white bg-black font-bold">
               Ricoh Theta SC2
             </th>
-            <th className="border border-gray-300 px-4 py-2 text-white bg-black font-bold">
+            <th className="border border-gray-300 md:px-4 px-2 py-2 text-white bg-black font-bold">
               Insta360 One X2
             </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="border border-gray-300 px-4 py-2">Photo Quality</td>
-            <td className="border border-gray-300 px-4 py-2">Good</td>
-            <td className="border border-gray-300 px-4 py-2">Good</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
+              Photo Quality
+            </td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">Good</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">Good</td>
           </tr>
           <tr className="bg-gray-50">
-            <td className="border border-gray-300 px-4 py-2">Video Quality</td>
-            <td className="border border-gray-300 px-4 py-2">
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
+              Video Quality
+            </td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
               No stabilization
             </td>
-            <td className="border border-gray-300 px-4 py-2">
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
               Better (with stabilization)
             </td>
           </tr>
           <tr>
-            <td className="border border-gray-300 px-4 py-2">
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
               Image Resolution
             </td>
-            <td className="border border-gray-300 px-4 py-2">14 MP</td>
-            <td className="border border-gray-300 px-4 py-2">18 MP</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">14 MP</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">18 MP</td>
           </tr>
           <tr>
-            <td className="border border-gray-300 px-4 py-2">Image Format</td>
-            <td className="border border-gray-300 px-4 py-2">JPEG</td>
-            <td className="border border-gray-300 px-4 py-2">RAW and JPEG</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
+              Image Format
+            </td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">JPEG</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
+              RAW and JPEG
+            </td>
           </tr>
           <tr>
-            <td className="border border-gray-300 px-4 py-2">
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
               Indoor Performance
             </td>
-            <td className="border border-gray-300 px-4 py-2">Excellent</td>
-            <td className="border border-gray-300 px-4 py-2">Good</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
+              Excellent
+            </td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">Good</td>
           </tr>
           <tr>
-            <td className="border border-gray-300 px-4 py-2">
+            <td className="border border-gray-300 md:px-4 px-2 py-2">
               Memory Card Slot
             </td>
-            <td className="border border-gray-300 px-4 py-2">No</td>
-            <td className="border border-gray-300 px-4 py-2">Yes</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">No</td>
+            <td className="border border-gray-300 md:px-4 px-2 py-2">Yes</td>
           </tr>
         </tbody>
       </table>
--- src/modules/components/SectionComparison.tsx
+++ src/modules/components/SectionComparison.tsx
@@ -18,20 +18,29 @@ function SectionComparison({ scroll }: Props) {
             <p>Ricoh Theta SC 2</p>
             <p>VS</p>
             <p>Insta360 One X2</p>
-            <Image
-              src="/assets/Ricoh.jpeg"
-              alt="Ricoh"
-              width={200}
-              height={200}
-              className="object-contain absolute left-20 -top-60 -rotate-[30deg]"
-            />
-            <Image
-              src="/assets/Insta360.webp"
-              alt="Ricoh"
-              width={200}
-              height={200}
-              className="object-contain absolute right-20 -bottom-60 rotate-[30deg]"
-            />
+            <div className=" absolute left-10 md:left-20 -top-72 md:-top-60 -rotate-[30deg]">
+              <div className="relative md:w-[200px] md:h-[200px] w-[100px] h-[100px]">
+                <Image
+                  src="/assets/Ricoh.jpeg"
+                  alt="Ricoh"
+                  fill
+                  sizes="(min-width: 768px) 300px, 100vw"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+
+            <div className=" absolute right-10 md:right-20 -bottom-72 md:-bottom-60 rotate-12 md:rotate-[30deg]">
+              <div className="relative md:w-[200px] md:h-[200px] w-[100px] h-[100px]">
+                <Image
+                  src="/assets/Insta360.webp"
+                  alt="Insta360"
+                  fill
+                  sizes="(min-width: 768px) 300px, 100vw"
+                  className="object-contain"
+                />
+              </div>
+            </div>
           </div>
         </SectionLayout>
       </ParallaxLayer>
@@ -44,6 +53,13 @@ function SectionComparison({ scroll }: Props) {
               alt="photo quality comparison"
               className="object-contain w-[1280px] h-auto"
             />
+            <a
+              href="https://spotbrowser.com/artykuly/ricoh-theta-cs2-vs-insta-360-one-x2/"
+              target="_blank"
+              className="mt-3 underline font-medium text-base text-center w-full"
+            >
+              Source
+            </a>
           </div>
         </SectionLayout>
       </ParallaxLayer>
@@ -51,20 +67,22 @@ function SectionComparison({ scroll }: Props) {
         <SectionLayout>
           <div className="flex flex-col w-full text-2xl md:text-4xl font-bold items-start">
             <p className="mb-5 md:mb-10">Video Quality</p>
-            <iframe
-              width="1280"
-              height="600"
-              src={`https://www.youtube.com/embed/kUpszwHhAmY`}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
+            <div className="w-full h-auto md:w-[1280px] md:h-[600px]">
+              <iframe
+                width="100%"
+                height="100%"
+                src={`https://www.youtube.com/embed/kUpszwHhAmY`}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
           </div>
         </SectionLayout>
       </ParallaxLayer>
       <ParallaxLayer offset={5} speed={0.1} onClick={() => scroll(6)}>
         <SectionLayout>
-          <div className="flex flex-col w-full text-2xl md:text-4xl  items-start">
+          <div className="flex flex-col w-full text-xl md:text-4xl items-start">
             <p className="mb-5 md:mb-10 font-bold">Comparison Table</p>
             <ComparisonTable />
           </div>
